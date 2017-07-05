823e9dc719a61a7ba59ef70d7f4c47dbc05e31be
sharafian <ben@sharafian.me>
2017-07-05 20:49:50 +0200

    add prefix and method to RPC call

--- connector.sh
+++ connector.sh
@@ -5,9 +5,10 @@ function get_incoming () {
   cat 'in.log'
 }
 
-# (json) -> (condition, expiry, ILP, amount)
+# (json) -> (id, condition, expiry, ILP, amount)
 function extract_ilp () {
   while read json; do
+    echo -n "$(echo "$json" | jq -r '.id') "
     echo -n "$(echo "$json" | jq -r '.executionCondition') "
     echo -n "$(echo "$json" | jq -r '.expiresAt') "
     echo -n "$(echo "$json" | jq -r '.ilp') "
@@ -15,15 +16,15 @@ function extract_ilp () {
   done
 }
 
-# (condition, expiry, ILP, amount) -> (condition, expiry, ILP, address, amount)
+# (id, condition, expiry, ILP, amount) -> (id, condition, expiry, ILP, address, amount)
 function parse_ilp () {
   while read packet; do
-    # write channels 1, 2, and 3 unaltered
-    echo -n "$(echo $packet | cut -f 1,2,3 -d' ') "
+    # write channels 1, 2, 3, and 4 unaltered
+    echo -n "$(echo $packet | cut -f 1,2,3,4 -d' ') "
 
     # write channel 4 as the parsed address
     echo "$packet" \
-      | cut -f 3 -d' ' \
+      | cut -f 4 -d' ' \
       | base64 -D \
       | hexdump -v -e '/1 "%02x"' \
       | xargs -n 1 -I {} echo "{}" \
@@ -31,14 +32,14 @@ function parse_ilp () {
       | xargs -n 2 bash -c 'echo ${1:0:$((2 * 16#$0))}' \
       | xxd -r -p
 
-    # write channel 5 as the amount
+    # write channel 6 as the amount
     echo -n ' '
     echo "$packet" \
-      | cut -f 4 -d' '
+      | cut -f 5 -d' '
   done
 }
 
-# (condition, expiry, ILP, address, amount) -> (condition, expiry, ILP, amount, nextHop, RPC)
+# (id, condition, expiry, ILP, address, amount) -> (id, condition, expiry, ILP, amount, nextHop, RPC)
 function get_routing_info () {
   # use a bash while loop to get a matching prefix
   function prefix_match () {
@@ -49,22 +50,29 @@ function get_routing_info () {
   }
   
   while read info; do
-    # write channels 1, 2, 3, and 5 unaltered
-    echo -n "$(echo $info | cut -f 1,2,3,5 -d' ') "
+    # write channels 1, 2, 3, 4, and 6 unaltered
+    echo -n "$(echo $info | cut -f 1,2,3,4,6 -d' ') "
 
-    # turn channel 4 into (nextHop, RPC)
+    # turn channel 5 into (nextHop, RPC)
     echo "$info" \
-      | cut -f 4 -d' ' \
+      | cut -f 5 -d' ' \
       | prefix_match \
       | cut -f 2,3 -d' '
   done
 }
 
-# (condition, expiry, ILP, amount, nextHop, RPC) -> network
+# (id, condition, expiry, ILP, amount, nextHop, RPC) -> network
 function post_destination_transfer () {
   while read info; do
-    echo $info \
-      | xargs -n 6 bash -c 'curl -X POST "$5" -H "Content-Type: application/json" -d "{\"executionCondition\":\"$0\",\"expiresAt\":\"$1\",\"ilp\":\"$2\",\"amount\":\"$3\",\"to\":\"$4\"}"'
+    echo "$info $(echo $info | cut -d' ' -f 6 | sed -e 's/\(.*\)\..*/\1./')" \
+      | xargs -n 8 bash -c 'curl -X POST "$6?prefix=$7&method=send_transfer" -H "Content-Type: application/json" -d "{\"id\":\"$0\",\"executionCondition\":\"$1\",\"expiresAt\":\"$2\",\"ilp\":\"$3\",\"amount\":\"$4\",\"to\":\"$5\"}"'
+  done
+}
+
+# can be inserted anywhere to view that part of the pipe
+function inspector () {
+  while read info; do
+    echo $info 1>&2
   done
 }
 
@@ -73,7 +81,3 @@ get_incoming \
   | parse_ilp \
   | get_routing_info \
   | post_destination_transfer
-  # | xargs -I {} -n 1 grep {} routing.txt \
-  # | cut -d' ' -f 1,2,3 \
-  # | xargs -n 3 bash -c 'curl -X POST "$2" -d "to=$1&ilp=$0" -H "Content-Type=application/json"'
-  # | xargs -n 3 echo
